8cab09f4b34d8d47e0fd6ff6dcef48fcd850c434
cevier.wei <cevier.wei@ideamake.cn>
2023-02-13 17:58:32 +0800

    单点登入

--- src/main/java/com/cevier/sso/controller/SSOController.java
+++ src/main/java/com/cevier/sso/controller/SSOController.java
@@ -1,12 +1,15 @@
 package com.cevier.sso.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.cevier.sso.pojo.User;
 import com.cevier.sso.service.UserService;
 import com.cevier.sso.utils.RedisUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
@@ -26,6 +29,32 @@ public class SSOController {
 
     @Resource
     private RedisUtils redisUtils;
+
+    @GetMapping("/login")
+    public String login(@RequestParam String returnUrl,
+                        Model model,
+                        HttpServletRequest request,
+                        HttpServletResponse response) {
+        var token = getCookie(request, "loginInfo");
+
+        // 验证token
+        if (StringUtils.hasText(token)) {
+            String userId = redisUtils.get("token:" + token);
+            if (StringUtils.hasText(userId)) {
+                var userInfo = redisUtils.get("userInfo:" + userId);
+                if (StringUtils.hasText(userInfo)) {
+                    // 生成临时登入凭证
+                    var ticket = UUID.randomUUID().toString();
+                    redisUtils.set("ticket:" + ticket, token, 600);
+                    return "redirect:" + returnUrl + "?ticket=" + ticket;
+                }
+            }
+        }
+
+        model.addAttribute("returnUrl", returnUrl);
+        return "login";
+    }
+
 
     @PostMapping("/doLogin")
     public String doLogin(String username,
@@ -64,6 +93,55 @@ public class SSOController {
         return "hello";
     }
 
+
+    /**
+     * 验证ticket
+     */
+    @GetMapping("/verify-ticket")
+    @ResponseBody
+    public boolean verifyTicket(@RequestParam String ticket, HttpServletRequest request, HttpServletResponse response) {
+
+        if (!StringUtils.hasText(ticket)) {
+            return false;
+        }
+
+        var token = redisUtils.get("ticket:" + ticket);
+        if (!StringUtils.hasText(token)) {
+            return false;
+        }
+
+        var userId = redisUtils.get("token:" + token);
+        if (!StringUtils.hasText(userId)) {
+            return false;
+        }
+
+        var userInfo = redisUtils.get("userInfo:" + userId);
+        if (!StringUtils.hasText(userInfo)) {
+            return false;
+        }
+
+        setCookie("userInfo", userInfo, response);
+        return true;
+    }
+
+
+    /**
+     * 验证ticket
+     */
+    @GetMapping("/logout")
+    @ResponseBody
+    public String logout(@RequestParam String userId, HttpServletRequest request, HttpServletResponse response) {
+        // 删除token
+        var token = getCookie(request, "loginInfo");
+        redisUtils.del("token:" + token);
+        // 清除cookie
+        deleteCookie("loginInfo", response);
+        // 清除全局会话
+        redisUtils.del("userInfo:" + userId);
+        return "bye";
+    }
+
+
     private void setCookie(String key,
                            String val,
                            HttpServletResponse response) {
@@ -71,6 +149,33 @@ public class SSOController {
         Cookie cookie = new Cookie(key, val);
         cookie.setDomain("sso.com");
         cookie.setPath("/");
+        response.addCookie(cookie);
+    }
+    private String getCookie(HttpServletRequest request, String key) {
+
+        Cookie[] cookieList = request.getCookies();
+        if (cookieList == null || StringUtils.hasText(key)) {
+            return null;
+        }
+
+        String cookieValue = null;
+        for (int i = 0 ; i < cookieList.length; i ++) {
+            if (cookieList[i].getName().equals(key)) {
+                cookieValue = cookieList[i].getValue();
+                break;
+            }
+        }
+
+        return cookieValue;
+    }
+
+    private void deleteCookie(String key,
+                              HttpServletResponse response) {
+
+        Cookie cookie = new Cookie(key, null);
+        cookie.setDomain("sso.com");
+        cookie.setPath("/");
+        cookie.setMaxAge(-1);
         response.addCookie(cookie);
     }
 }
